8518d9e5debe3bd55b928ef22fb48abb58900acb
jesslalanne <jessica.lalanne@gmail.com>
2018-09-02 16:41:41 +0200

    Contact Form Style

--- app/views/Contact.js
+++ app/views/Contact.js
@@ -4,94 +4,96 @@ import { Header } from "../sections/Header";
 import { createStackNavigator } from "react-navigation";
 
 export class Contact extends React.Component {
-	static navigationOptions = {
-      		header: null
+  static navigationOptions = {
+    header: null
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: "Message",
+      name: "Name",
+      email: "Email"
     };
-    constructor(props) {
-        super(props);
-        this.state = {
-            msg: 'Enter Message',
-            name: 'Enter Name',
-            email: 'Enter your Email Address'
-         }    
-    }
+  }
 
-    clearFields=()=> this.setState({name:'',msg:'', email:''});
+  clearFields = () => this.setState({ name: "", msg: "", email: "" });
 
-    sendMessage=()=> { 
-        Alert.alert(this.state.name,this.state.msg);
-        this.props.navigation.goBack();
-    };
+  sendMessage = () => {
+    Alert.alert(this.state.name, this.state.msg);
+    this.props.navigation.goBack();
+  };
 
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <Header navigate={navigate} message="Press to Login" />
+        <Text style={styles.heading}>Contact Us</Text>
 
-    render() {
-        const { navigate } = this.props.navigation;
-        return (
-          <View style={styles.container}>
-            <Header navigate = {navigate} message = 'Press to Login' />
-            <Text style={styles.heading}>Contact Us</Text>
-              
-            <TextInput 
-                style={styles.inputs} 
-                onChangeText={(text) => this.setState({name: text})}
-                value={this.state.name}
-            />
+        <TextInput style={styles.inputs} onChangeText={text => this.setState({ name: text })} value={this.state.name} />
 
-            <TextInput 
-                style={styles.multiInput} 
-                onChangeText={(text) => this.setState({msg: text})}
-                value={this.state.msg}
-                multiline = {true}
-                numberOfLines = {4}
-            />
+        <TextInput
+          style={styles.multiInput}
+          onChangeText={text => this.setState({ msg: text })}
+          value={this.state.msg}
+          multiline={true}
+          numberOfLines={4}
+        />
 
-            <TextInput 
-                style={styles.inputs} 
-                onChangeText={(text) => this.setState({email: text})}
-                value={this.state.email}
-            />
+        <TextInput
+          style={styles.inputs}
+          onChangeText={text => this.setState({ email: text })}
+          value={this.state.email}
+        />
 
-            <TouchableHighlight onPress={this.sendMessage} underlayColor='#31e981'>
-                <Text style = {styles.buttons}>
-                    Send Message
-                </Text>
-            </TouchableHighlight>
-            
-            <TouchableHighlight onPress={this.clearFields} underlayColor='#31e981'>
-              <Text style = {styles.buttons}>
-                Reset Form
-              </Text>
-            </TouchableHighlight>
+        <TouchableHighlight onPress={this.sendMessage} underlayColor="#31e981">
+          <Text style={styles.buttons}>Send Message</Text>
+        </TouchableHighlight>
 
-          </View>
-        );
-    }
-};
-
-
+        <TouchableHighlight onPress={this.clearFields} underlayColor="#31e981">
+          <Text style={styles.buttons}>Reset Form</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        paddingBottom: '45%'
-    },
-    heading: {
-        fontSize: 16,
-        flex: 1
-    },
-    inputs:{
-        flex: 1,
-        width: '80%',
-        padding: 10
-    },
-    multiInput:{
-        flex: 2,
-        width: '90%',
-        paddingTop:20
-    },
-    buttons:{
-        marginTop: 15,
-        fontSize: 16
-    }
+  container: {
+    flex: 1,
+    alignItems: "center",
+    paddingBottom: "45%"
+  },
+  heading: {
+    fontSize: 16,
+    flex: 1,
+    paddingTop: 10
+  },
+  inputs: {
+    flex: 1,
+    width: "80%",
+    padding: 10,
+    marginBottom: 5,
+    borderColor: "#000000",
+    borderWidth: 1,
+    borderRadius: 5
+  },
+  multiInput: {
+    flex: 2,
+    width: "80%",
+    paddingTop: 20,
+    paddingLeft: 10,
+    marginBottom: 5,
+    borderColor: "#000000",
+    borderWidth: 1,
+    borderRadius: 5
+  },
+  buttons: {
+    marginTop: 15,
+    fontSize: 16,
+    borderColor: "#000000",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10
+  }
 });
